320f75a6b5afaae1cc20c2e7b9cde12732e13f45
dberg258 <40529001+dberg258@users.noreply.github.com>
2018-11-26 18:09:00 -0500

    Added exception handling.

--- app.py
+++ app.py
@@ -6,12 +6,25 @@ app = Flask(__name__)
 
 @app.route('/pokemon/<data>', methods=['GET'])
 def main(data):
-    request = requests.get('https://pokeapi.co/api/v2/pokemon/' + data)
+    try:
+        request = requests.get('https://pokeapi.co/api/v2/pokemon/' + data)
     
-    if data.isdigit():
-        data = "The Pokemon with id " + data + " is " + request['name'].capitalize() + "."
-    else:
-        data = data.capitalize() + " has id " + str(request['id']) + "."
+        if data.isdigit():
+            data = "The Pokemon with id " + data + " is " + request['name'].capitalize() + "."
+        else:
+            data = data.capitalize() + " has id " + str(request['id']) + "."
+    
+    except requests.exceptions.Timeout as e:
+        data = "Cannot find your Pokemon at this time. :("
+        print(e)
+    except requests.exceptions.TooManyRedirects as e:
+        data = "Cannot find your Pokemon at this time. :("
+        print(e)
+    except json.decoder.JSONDecodeError:
+        if data.isdigit():
+            data = "Id does not match any Pokemon :("
+        else:
+            data = "Pokemon does not exist. Make sure spelling is correct (no capital letters) :("
 
     return render_template('index.html', name=data)
 
