84cca74e35f698eab52d47d1879009ee0786baac
dberg258 <40529001+dberg258@users.noreply.github.com>
2018-11-26 17:44:51 -0500

    Inserted PokeAPI call.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/pokemon/<data>', methods=['GET'])
 def main():
-    return render_template('index.html')
+    request = requests.get('https://pokeapi.co/api/v2/pokemon/' + data)
+    return render_template('index.html', name=request)
 
 
 if __name__ == '__main__':
